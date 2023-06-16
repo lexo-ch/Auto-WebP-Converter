8a75d3b1ef618d3f3ebaaf1cc05d0099f20397c8
LEXO - Tim Fürer <90773991+lexo-tfuerer@users.noreply.github.com>
2023-06-16 14:29:57 +0200

    chore(version): Bump to 2.0.1

--- lexo-auto-webp-convert.php
+++ lexo-auto-webp-convert.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://www.lexo.ch
  * Description: Automatically converts JPG and PNG images uploaded to the WordPress media library to the WebP format.
  * Author: LEXO
- * Version: 2.0.0
+ * Version: 2.0.1
  * Author URI: https://www.lexo.ch
  */
 
